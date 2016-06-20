99581232bdeafdce3c9490686398f9f4c02ba539
Bolke de Bruin <bolke@xs4all.nl>
2016-06-20 21:20:43 +0200

    MiniKdc integration

--- scripts/ci/setup_kdc.sh
+++ scripts/ci/setup_kdc.sh
@@ -1,30 +1,27 @@
 #!/usr/bin/env bash
-MINIKDC_VERSION=2.7.1
+set -o verbose
+
+MINIKDC_VERSION=1.0-SNAPSHOT
 MINIKDC_HOME=/tmp/minikdc
 MINIKDC_CACHE=${CACHE}/minikdc
+MINICLUSTER_URL=https://github.com/bolkedebruin/minikdc/files/324103/minikdc-${MINIKDC_VERSION}-bin.zip
+
+PRINCIPALS="airflow airflow-webserver airflow-scheduler airflow-worker"
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# setup cache dir
+mkdir -p ${TRAVIS_CACHE}/minikdc
+
 # Setup MiniKDC environment
 mkdir -p ${MINIKDC_HOME}
 mkdir -p ${MINIKDC_CACHE}
 
-URL=http://search.maven.org/remotecontent?filepath=org/apache/ivy/ivy/2.3.0/ivy-2.3.0.jar
-echo "Downloading ivy"
-curl -z ${MINIKDC_CACHE}/ivy.jar -o ${MINIKDC_CACHE}/ivy.jar -L ${URL}
+echo "Downloading and unpacking MiniKDC"
+curl -z ${TRAVIS_CACHE}/minikdc/minikdc.zip -o ${TRAVIS_CACHE}/minikdc/minikdc.zip -L ${MINICLUSTER_URL}
+unzip ${TRAVIS_CACHE}/minikdc/minikdc.zip -d /tmp
 
-if [ $? != 0 ]; then
-    echo "Failed to download ivy"
-    exit 1
-fi
+echo "Path = ${PATH}"
 
-echo "Getting minikdc dependencies"
-java -jar ${MINIKDC_CACHE}/ivy.jar -dependency org.apache.hadoop hadoop-minikdc ${MINIKDC_VERSION} \
-           -cache ${MINIKDC_CACHE} \
-           -retrieve "${MINIKDC_CACHE}/lib/[artifact]-[revision](-[classifier]).[ext]"
-
-if [ $? != 0 ]; then
-    echo "Failed to download dependencies for minikdc"
-    exit 1
-fi
-
-cp -r ${MINIKDC_CACHE}/* ${MINIKDC_HOME}/
-
-mkdir -p ${MINIKDC_HOME}/work
+java -cp "/tmp/minikdc-${MINIKDC_VERSION}/*" org.apache.airflow.MiniKdc \
+/tmp ${DIR}/minikdc.properties /tmp/minikdc.keytab \${PRINCIPALS} &
